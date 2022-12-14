0942fec0d963c79cd1c2ceb27b629332871f790b
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-13 22:06:38 -0300

    stack nestjs added

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,125 +1,150 @@
-import { SiJavascript, SiStyledcomponents, SiPostgresql, SiTypescript, SiJest, SiMongodb, SiHeroku, SiRedux, SiJson, SiSwagger, SiVercel, SiExpress, SiPrisma } from 'react-icons/si'
-import { FaReact, FaGithub, FaPython, FaDocker, FaNodeJs } from 'react-icons/fa'
-import { DiCss3, DiDjango, DiMysql } from 'react-icons/di'
-import { AiFillHtml5 } from 'react-icons/ai'
-
+import {
+  SiJavascript,
+  SiStyledcomponents,
+  SiPostgresql,
+  SiTypescript,
+  SiJest,
+  SiMongodb,
+  SiHeroku,
+  SiRedux,
+  SiJson,
+  SiSwagger,
+  SiVercel,
+  SiExpress,
+  SiPrisma,
+  SiNestjs,
+} from "react-icons/si";
+import {
+  FaReact,
+  FaGithub,
+  FaPython,
+  FaDocker,
+  FaNodeJs,
+} from "react-icons/fa";
+import { DiCss3, DiDjango, DiMysql } from "react-icons/di";
+import { AiFillHtml5 } from "react-icons/ai";
 
 const stack_data = [
-	{
-		title: 'HTML',
-		img: AiFillHtml5,
-		color: '#e34c26'
-	},
-	{
-		title: 'CSS',
-		img: DiCss3,
-		color: '#07B'
-	},
-	{
-		title: 'JavaScript',
-		img: SiJavascript,
-		color: '#F7DF1E'
-	},
-	{
-		title: 'TypeScript',
-		img: SiTypescript,
-		color: '#0D61A9'
-	},
-	{
-		title: 'React',
-		img: FaReact,
-		color: '#61DBFB'
-	},
-	{ 
-		title: 'React Native',
-		img: FaReact,
-		color: '#61DBFB'
-	},
-	{
-		title: 'Styled Components',
-		img: SiStyledcomponents,
-		color: '#868E96'
-	},
-	{
-		title: 'Redux',
-		img: SiRedux,
-		color: '#764abc'
-	},
-	{
-		title: 'Node JS',
-		img: FaNodeJs,
-		color: '#21A366'
-	},
-	{
-		title: 'Express',
-		img: SiExpress,
-		color: '#000000'
-	},
-	{
-		title: 'Prisma',
-		img: SiPrisma,
-		color: '#21A366'
-	},
-	{
-		title: 'JSON',
-		img: SiJson,
-		color: '#DEDEDE'
-	},
-	{
-		title: 'Python',
-		img: FaPython,
-		color: '#868E96'
-	},
-	{
-		title: 'Django',
-		img: DiDjango,
-		color: '#004D40'
-	},
-	{
-		title: 'MySQL',
-		img: DiMysql,
-		color: '#3079D6'
-	},
-	{
-		title: 'Postgresql',
-		img: SiPostgresql,
-		color: '#3079D6'
-	},
-	{
-		title: 'Git Hub',
-		img: FaGithub,
-		color: '#4C4C4C'
-	},
-	{
-		title: 'Docker',
-		img: FaDocker,
-		color: '#2395EC'
-	},
-	{
-		title: 'Jest',
-		img: SiJest,
-		color: '#99415B'
-	},
-	{
-		title: 'MongoDB',
-		img: SiMongodb,
-		color: '#3FA037'
-	},
-	{
-		title: 'Swagger',
-		img: SiSwagger,
-		color: '#63db2a'
-	},
-	{
-		title: 'Heroku',
-		img: SiHeroku,
-		color: '#60467e'
-	},
-	{
-		title: 'Vercel',
-		img: SiVercel,
-		color: '#fff'
-	},
-]
+  {
+    title: "HTML",
+    img: AiFillHtml5,
+    color: "#e34c26",
+  },
+  {
+    title: "CSS",
+    img: DiCss3,
+    color: "#07B",
+  },
+  {
+    title: "JavaScript",
+    img: SiJavascript,
+    color: "#F7DF1E",
+  },
+  {
+    title: "TypeScript",
+    img: SiTypescript,
+    color: "#0D61A9",
+  },
+  {
+    title: "React",
+    img: FaReact,
+    color: "#61DBFB",
+  },
+  {
+    title: "React Native",
+    img: FaReact,
+    color: "#61DBFB",
+  },
+  {
+    title: "Styled Components",
+    img: SiStyledcomponents,
+    color: "#868E96",
+  },
+  {
+    title: "Redux",
+    img: SiRedux,
+    color: "#764abc",
+  },
+  {
+    title: "Nestjs",
+    img: SiNestjs,
+    color: "#ea2845",
+  },
+  {
+    title: "Node JS",
+    img: FaNodeJs,
+    color: "#21A366",
+  },
+  {
+    title: "Express",
+    img: SiExpress,
+    color: "#000000",
+  },
+  {
+    title: "Prisma",
+    img: SiPrisma,
+    color: "#21A366",
+  },
+  {
+    title: "JSON",
+    img: SiJson,
+    color: "#DEDEDE",
+  },
+  {
+    title: "Python",
+    img: FaPython,
+    color: "#868E96",
+  },
+  {
+    title: "Django",
+    img: DiDjango,
+    color: "#004D40",
+  },
+  {
+    title: "MySQL",
+    img: DiMysql,
+    color: "#3079D6",
+  },
+  {
+    title: "Postgresql",
+    img: SiPostgresql,
+    color: "#3079D6",
+  },
+  {
+    title: "Git Hub",
+    img: FaGithub,
+    color: "#4C4C4C",
+  },
+  {
+    title: "Docker",
+    img: FaDocker,
+    color: "#2395EC",
+  },
+  {
+    title: "Jest",
+    img: SiJest,
+    color: "#99415B",
+  },
+  {
+    title: "MongoDB",
+    img: SiMongodb,
+    color: "#3FA037",
+  },
+  {
+    title: "Swagger",
+    img: SiSwagger,
+    color: "#63db2a",
+  },
+  {
+    title: "Heroku",
+    img: SiHeroku,
+    color: "#60467e",
+  },
+  {
+    title: "Vercel",
+    img: SiVercel,
+    color: "#fff",
+  },
+];
 
-export { stack_data }
+export { stack_data };
